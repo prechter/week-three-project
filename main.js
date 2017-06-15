3361e4f578c1d80d2a93095bc75be7bf2b607133
Michael Prechter <m.r.prechter@gmail.com>
2017-06-15 17:16:22 -0400

    final submit, reworked JS

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-let nums = document.getElementsByClassName("num");
 let displayArray = [];
+let displayResult = [];
+let nums = document.getElementsByClassName("num");
+let display = document.getElementById("display");
+let ops = document.getElementsByClassName("op")
+let equals = document.getElementById("equals");
+let clear = document.getElementById("clear");
 
-for (var i = 0; i < nums.length; i++) {
-  nums[i].addEventListener("click", displayNum);
-  function displayNum() {
-    let display = document.getElementById("display");
-    displayArray.push(this.value);
-    display.value = displayArray.join("");
-    console.log(displayArray);
-  }
-}
+  for (var num of nums) {
+    num.addEventListener("click", function (){
+    displayArray += this.value;
+    console.log(this.value);
+    display.value = displayArray;
+  })};
+
+  for (var op of ops){
+    op.addEventListener("click", function (){
+    displayArray += this.value;
+    display.value = displayArray;
+  })};
+
+  equals.addEventListener("click", function (){
+    displayResult = eval(displayArray);
+    display.value = displayResult;
+  })
+
+  clear.addEventListener("click", function (){
+    display.value = [];
+    displayArray = [];
+  })
